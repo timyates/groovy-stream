6ecec905b941b1503a58a6299063f3c13b641790
Tim Yates <tyates@picr.man.ac.uk>
2012-04-27 16:52:42 +0100

    Added test for large ranges
    Can't be larger than this currently with Groovy 1.8.6

--- src/test/groovy/groovy/stream/IteratorTests.groovy
+++ src/test/groovy/groovy/stream/IteratorTests.groovy
@@ -28,4 +28,21 @@ public class IteratorTests extends spock.lang.Specification {
     stream.exhausted
     stream.streamIndex == 4
   }
+
+  def "Use of the Stream stopper with large ranges"() {
+    setup:
+    // Ranges as of Groovy 1.8.6 can't be > Integer.MAX_VALUE
+    // in length else iterator() fails
+    def eternal = (1..2147483647).iterator()
+    def stream = Stream.from eternal where { it < 5 ?: STOP }
+
+    when:
+    def result = stream.collect()
+
+    then:
+    result == [ 1, 2, 3, 4 ]
+    eternal.next() == 6
+    stream.exhausted
+    stream.streamIndex == 3
+  }
 }
