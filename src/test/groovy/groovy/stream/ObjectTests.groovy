9dd113d286fdb92efc852d06cf42b0dba3ecd844
Tim Yates <tyates@picr.man.ac.uk>
2012-04-30 12:08:45 +0100

    Added Object Tests

---
+++ src/test/groovy/groovy/stream/ObjectTests.groovy
@@ -0,0 +1,14 @@
+package groovy.stream
+
+public class ObjectTests extends spock.lang.Specification {
+  def "test obj appender"() {
+    setup:
+    def stream = Stream.from { 1 }
+
+    when:
+    def result = stream.take( 4 ).collect()
+
+    then:
+    result == [ 1, 1, 1, 1 ]
+  }
+}
