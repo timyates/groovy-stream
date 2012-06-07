f1b5685a88fe0f2f8d44f9fc5359b5c533849cfb
Tim Yates <tim.yates@gmail.com>
2012-06-07 11:52:21 +0100

    License comment added

--- src/main/groovy/groovy/stream/Stream.groovy
+++ src/main/groovy/groovy/stream/Stream.groovy
@@ -1,14 +1,32 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package groovy.stream
 
 public class Stream<T> implements StreamInterface<T> {
   StreamInterface wrapped
+
   private static enum StreamType { MAP, OTHER }
+
   private StreamType type
-  public boolean hasNext()        { wrapped.hasNext() }
-  public T       next()           { wrapped.next() }
-  public void    remove()         { wrapped.remove() }
-  public boolean isExhausted()    { wrapped.exhausted }
-  public int     getStreamIndex() { wrapped.streamIndex }
+
+  public  boolean    hasNext()        { wrapped.hasNext()   }
+  public  T          next()           { wrapped.next()      }
+  public  void       remove()         { wrapped.remove()    }
+  public  boolean    isExhausted()    { wrapped.exhausted   }
+  public  int        getStreamIndex() { wrapped.streamIndex }
 
   public static Stream from( Map a ) {
     new Stream( type:StreamType.MAP, wrapped:new MapStream( { a }, { true }, { it }, [:] ) )
