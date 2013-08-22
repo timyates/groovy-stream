5c8099c927e11a95296880818236689c37e2d29e
Tim Yates <tyates@picr.man.ac.uk>
2013-08-22 12:02:43 +0100

    Simple tests for chains of filters and maps

---
+++ src/test/groovy/groovy/stream/ChainTests.groovy
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package groovy.stream
+
+class ChainTests extends spock.lang.Specification {
+    def "filter map filter chain"() {
+        setup:
+            def stream = Stream.from 1..10 filter { it % 2 } map { it * 2 } filter { it % 3 }
+        when:
+            def result = stream.collect()
+        then:
+            result == [ 2, 10, 14 ]
+    }
+
+    def "map map filter chain"() {
+        setup:
+            def stream = Stream.from( x:1..2, y:3..4 )
+                               .map { [ x: x, y: y, z: x + y ] }
+                               .map { [ diff: Math.abs( x - y ), sum: z ] }
+                               .filter { diff > 2 }
+        when:
+            def result = stream.collect()
+        then:
+            result == [ [ diff: 3, sum: 5 ] ]
+    }
+}
