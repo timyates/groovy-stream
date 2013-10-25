38bfcf8f5f940fb7bf2f982bb8ff5021dba1d5ba
Tim Yates <tyates@picr.man.ac.uk>
2013-10-25 15:35:00 +0100

    4 spaces good, 2 spaces bad

--- src/test/groovy/groovy/stream/iterators/MapIteratorTests.groovy
+++ src/test/groovy/groovy/stream/iterators/MapIteratorTests.groovy
@@ -17,34 +17,34 @@
 package groovy.stream.iterators
 
 class MapIteratorTests extends spock.lang.Specification {
-  def "test map iterator"() {
-    setup:
-      def i = new MapIterator( [ tim:1..3, alice:4..6 ] )
+    def "test map iterator"() {
+        setup:
+            def i = new MapIterator( [ tim:1..3, alice:4..6 ] )
 
-    when:
-      def result = i.collect()
+        when:
+            def result = i.collect()
 
-    then:
-      result == [ [tim:1, alice:4], [tim:1, alice:5], [tim:1, alice:6],
-                  [tim:2, alice:4], [tim:2, alice:5], [tim:2, alice:6],
-                  [tim:3, alice:4], [tim:3, alice:5], [tim:3, alice:6] ]
-  }
+        then:
+            result == [ [tim:1, alice:4], [tim:1, alice:5], [tim:1, alice:6],
+                                    [tim:2, alice:4], [tim:2, alice:5], [tim:2, alice:6],
+                                    [tim:3, alice:4], [tim:3, alice:5], [tim:3, alice:6] ]
+    }
 
-  def "Test NoSuchElementException"() {
-    setup:
-      def i = new MapIterator( [ tim:1..3, alice:4..6 ] )
+    def "Test NoSuchElementException"() {
+        setup:
+            def i = new MapIterator( [ tim:1..3, alice:4..6 ] )
 
-    when:
-      def result = i.collect()
+        when:
+            def result = i.collect()
 
-    then:
-      result == [ [tim:1, alice:4], [tim:1, alice:5], [tim:1, alice:6],
-                  [tim:2, alice:4], [tim:2, alice:5], [tim:2, alice:6],
-                  [tim:3, alice:4], [tim:3, alice:5], [tim:3, alice:6] ]
-    when:
-      i.next()
+        then:
+            result == [ [tim:1, alice:4], [tim:1, alice:5], [tim:1, alice:6],
+                                    [tim:2, alice:4], [tim:2, alice:5], [tim:2, alice:6],
+                                    [tim:3, alice:4], [tim:3, alice:5], [tim:3, alice:6] ]
+        when:
+            i.next()
 
-    then:
-      thrown java.util.NoSuchElementException
-  }
+        then:
+            thrown java.util.NoSuchElementException
+    }
 }
