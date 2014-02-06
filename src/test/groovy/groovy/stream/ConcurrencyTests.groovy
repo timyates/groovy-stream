80f69d920aa96d45c2d5c4841f879657dd40de87
Tim Yates <tyates@picr.man.ac.uk>
2014-02-06 14:34:54 +0000

    Test for concurrent reads

---
+++ src/test/groovy/groovy/stream/ConcurrencyTests.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package groovy.stream
+
+class ConcurrencyTests extends spock.lang.Specification {
+
+    def "Should allow multiple threads reading from the map stream"() {
+        setup:
+            def s = Stream.from x:1..10, y:1..10
+
+            def c = []
+
+            def p = { v ->
+                synchronized( c ) {
+                    c << v
+                }
+            }
+
+            def g = { name -> new Thread( { -> while( s.hasNext() ) p( s.next() ) } ) }
+
+            def t = [ g( 'a' ), g( 'b' ) ]
+
+        when:
+            t*.start()
+            t*.join()
+
+        then:
+
+            c.size() == 100
+            c.unique().size() == 100
+    }
+}
